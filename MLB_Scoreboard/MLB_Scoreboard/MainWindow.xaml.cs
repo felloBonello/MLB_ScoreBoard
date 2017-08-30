e714885581f9f45cb998a9a2ccd3f66f9169304a
Bonello <j_bonello2@fanshawec.ca>
2017-08-30 15:21:25 -0400

    Initial commit, review needed

---
+++ MLB_Scoreboard/MLB_Scoreboard/MainWindow.xaml.cs
@@ -0,0 +1,231 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MLB_Scoreboard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Rootobject> thisYearsDates;
+        Year_Array year_a;
+        Rootobject SelectedDate;
+        int selectedYear = -1;
+
+        public MainWindow()
+        {
+            InitializeComponent();           
+            LoadData();           
+            LoadComboBox();
+            datesDisplay.DataContext = thisYearsDates;
+        }
+
+        private void LoadComboBox()
+        {
+            for (var i = 2000; i <= DateTime.Now.Year; ++i)
+                year_cbo.Items.Add(i);
+        }
+
+        private void UpdateCache(Year y)
+        {
+            foreach(var date in y.data)
+            {
+                Games gameData = date.data.games;
+                Games fetchedData = fetchDate(new DateTime(int.Parse(gameData.year), int.Parse(gameData.month), int.Parse(gameData.day))).data.games;
+                if(fetchedData != null)
+                    if (!fetchedData.modified_date.Equals(gameData.modified_date))
+                        gameData = fetchedData;          
+            }
+        }
+
+        private void LoadData()
+        {
+            year_a = new Year_Array()
+            {
+                years = new List<Year>()
+            };
+            int currentYear = DateTime.Now.Year;
+            year_cbo.SelectedItem = currentYear;
+            thisYearsDates = new ObservableCollection<Rootobject>();
+            selectedYear = currentYear;
+            dateSelect.DisplayDateStart = new DateTime(currentYear, 1, 1);
+            dateSelect.DisplayDateEnd = new DateTime(currentYear, 12, 31);
+
+            if (File.Exists("MLB.json"))
+            {
+                string json = System.IO.File.ReadAllText("MLB.json");
+                year_a = JsonConvert.DeserializeObject<Year_Array>(json);
+
+                if (year_a.years != null)
+                    foreach (var y in year_a.years)
+                    {
+                        UpdateCache(y);
+                        if (y.active)
+                        {
+                            year_cbo.SelectedItem = y.year;
+                            thisYearsDates = y.data;
+                            y.active = false;
+                            selectedYear = y.year;
+                            dateSelect.DisplayDateStart = new DateTime(y.year, 1, 1);
+                            dateSelect.DisplayDateEnd = new DateTime(y.year, 12, 31);
+                            //dateSelect.SetCurrentValue(Calendar.SelectedDateProperty, new DateTime(y.year, 1, 1));
+                        }
+                    }
+            }
+            else
+            {
+                StreamWriter output = new StreamWriter("MLB.json");
+                output.Close();               
+            }             
+        }
+
+        private Rootobject fetchDate(DateTime date)
+        {
+            try
+            {
+                string url = @"http://gd2.mlb.com/components/game/mlb/year_" + date.Year + "/month_" + date.Month.ToString("00") + "/day_" + date.Day.ToString("00") + "/master_scoreboard.json";
+                WebClient client = new WebClient();
+                string json = client.DownloadString(url);
+                client.Dispose();
+                Rootobject mlbData = JsonConvert.DeserializeObject<Rootobject>(json);
+                if (mlbData.data.games.game == null)
+                    return null;
+                return mlbData;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return null;       
+        }
+
+        private void dateSelect_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(dateSelect.SelectedDate != null)
+            {
+                foreach (var date in thisYearsDates)
+                {
+                    DateTime d = new DateTime(int.Parse(date.data.games.year), int.Parse(date.data.games.month), int.Parse(date.data.games.day));
+                    if (d == dateSelect.SelectedDate)
+                    {
+                        SelectedDate = date;
+                        return;
+                    }
+                }
+                DateTime newDate = dateSelect.SelectedDate ?? default(DateTime);
+                SelectedDate = fetchDate(newDate);
+                if (SelectedDate != null)
+                    thisYearsDates.Add(SelectedDate);
+                else
+                    MessageBox.Show("There is no game data for the that day.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }            
+        }
+
+        private void UpdateYear()
+        {
+            Year updatedYear = new Year()
+            {
+                year = selectedYear,
+                active = false,
+                data = thisYearsDates
+            };
+            year_a.years.Add(updatedYear);
+        }
+
+        private void CheckForCurrentYearChanges()
+        {
+            foreach (var y in year_a.years)
+            {
+                if (y.year == selectedYear)
+                {                    
+                    if (y.data.Count != thisYearsDates.Count)
+                    {
+                        year_a.years.Remove(y);
+                        UpdateYear();
+                    }
+                    return;                       
+                }               
+            }
+            if(thisYearsDates.Count > 0)
+                UpdateYear();
+            else
+            {
+                thisYearsDates = new ObservableCollection<Rootobject>();
+                UpdateYear();
+            }
+        }
+        
+        private void findCurrentYear()
+        {
+            bool found = false;
+            foreach (var y in year_a.years)
+                if (y.year == selectedYear)
+                {
+                    thisYearsDates = y.data;
+                    found = true;
+                }
+            if (!found)
+                thisYearsDates = new ObservableCollection<Rootobject>();
+
+            datesDisplay.DataContext = thisYearsDates;
+            datesDisplay.Items.Refresh();
+        }
+
+        private void year_cbo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CheckForCurrentYearChanges();
+            selectedYear = int.Parse(year_cbo.SelectedValue.ToString());
+            findCurrentYear();
+
+            dateSelect.SelectedDate = null;
+            dateSelect.DisplayDateStart = new DateTime(selectedYear, 1, 1);
+            dateSelect.DisplayDateEnd = new DateTime(selectedYear, 12, 31);
+        }
+
+        private void setCurrentYearToActive()
+        {
+            foreach(var y in year_a.years)
+            {
+                if (y.year == selectedYear)
+                    y.active = true;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            CheckForCurrentYearChanges();
+            setCurrentYearToActive();
+            try
+            {
+                string json = JsonConvert.SerializeObject(year_a);
+                string path = AppDomain.CurrentDomain.BaseDirectory + "MLB.json";
+                StreamWriter output = new StreamWriter(path);
+                output.WriteLine(json);
+                output.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+ 
+    }
+}
---
+++ MLB_Scoreboard/MLB_Scoreboard/ScoreboardModel.cs
@@ -0,0 +1,445 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+
+namespace MLB_Scoreboard
+{
+    public class SingleValueArrayConverter<T> : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            object retVal = new Object();
+            if (reader.TokenType != JsonToken.Null)
+            {
+                if (reader.TokenType == JsonToken.StartObject)
+                {
+                    T instance = (T)serializer.Deserialize(reader, typeof(T));
+                    retVal = new List<T>() { instance };
+                }
+                else if (reader.TokenType == JsonToken.StartArray)
+                {
+                    retVal = serializer.Deserialize(reader, objectType);
+                }
+            }
+            else
+                retVal = new List<T>();
+
+            return retVal;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return false;
+        }
+    }
+
+    public class SingleValueObservableArrayConverter<T> : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value);          
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            object retVal = new Object();
+            if (reader.TokenType != JsonToken.Null)
+            {
+                if (reader.TokenType == JsonToken.StartObject)
+                {
+                    T instance = (T)serializer.Deserialize(reader, typeof(T));
+                    retVal = new ObservableCollection<T>() { instance };
+                }
+                else if (reader.TokenType == JsonToken.StartArray)
+                {
+                    retVal = serializer.Deserialize(reader, objectType);
+                }
+            }
+            else
+                retVal = new List<T>();
+
+            return retVal;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return false;
+        }
+    }
+
+    public class Year_Array
+    {
+        [JsonConverter(typeof(SingleValueArrayConverter<Year>))]
+        public List<Year> years { get; set; }
+    }
+
+    public class Year
+    {
+        public int year { get; set; }
+        public bool active { get; set; }
+        [JsonConverter(typeof(SingleValueObservableArrayConverter<Rootobject>))]
+        public ObservableCollection<Rootobject> data { get; set; }
+    }
+
+    public class Rootobject
+    {
+        
+        public override string ToString()
+        {
+            return data.games.year + "-" + data.games.month + "-" + data.games.day;
+        }
+        public DateTime getDate
+        {
+            get
+            {
+                return new DateTime(int.Parse(this.data.games.year), int.Parse(this.data.games.month), int.Parse(this.data.games.day));
+            }
+            set { }
+        }
+        public string subject { get; set; }
+        public string copyright { get; set; }
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public Games games { get; set; }
+    }
+
+    public class Games
+    {
+        public string next_day_date { get; set; }
+        public DateTime modified_date { get; set; }
+        public string month { get; set; }
+        public string year { get; set; }
+
+        [JsonConverter(typeof(SingleValueObservableArrayConverter<Game>))]
+        public ObservableCollection<Game> game { get; set; }
+        public string day { get; set; }
+    }
+
+    public class Game
+    {
+        public override string ToString()
+        {
+            return home_team_name + " vs. " + away_team_name;
+        }
+        public string game_type { get; set; }
+        public string double_header_sw { get; set; }
+        public string location { get; set; }
+        public string away_time { get; set; }
+        public Broadcast broadcast { get; set; }
+        public string time { get; set; }
+        public string home_time { get; set; }
+        public string home_team_name { get; set; }
+        public string description { get; set; }
+        public string original_date { get; set; }
+        public string home_team_city { get; set; }
+        public string venue_id { get; set; }
+        public string gameday_sw { get; set; }
+        public string away_win { get; set; }
+        public string home_games_back_wildcard { get; set; }
+        public Save_Pitcher save_pitcher { get; set; }
+        public string away_team_id { get; set; }
+        public string tz_hm_lg_gen { get; set; }
+        public Status status { get; set; }
+        public string home_loss { get; set; }
+        public string home_games_back { get; set; }
+        public string home_code { get; set; }
+        public string away_sport_code { get; set; }
+        public string home_win { get; set; }
+        public string time_hm_lg { get; set; }
+        public string away_name_abbrev { get; set; }
+        public string league { get; set; }
+        public string time_zone_aw_lg { get; set; }
+        public string away_games_back { get; set; }
+        public string home_file_code { get; set; }
+        public string game_data_directory { get; set; }
+        public string time_zone { get; set; }
+        public string away_league_id { get; set; }
+        public string home_team_id { get; set; }
+        public string day { get; set; }
+        public string time_aw_lg { get; set; }
+        public string away_team_city { get; set; }
+        public string tbd_flag { get; set; }
+        public string tz_aw_lg_gen { get; set; }
+        public string away_code { get; set; }
+        public Winning_Pitcher winning_pitcher { get; set; }
+        public Game_Media game_media { get; set; }
+        public string game_nbr { get; set; }
+        public string time_date_aw_lg { get; set; }
+        public string away_games_back_wildcard { get; set; }
+        public string scheduled_innings { get; set; }
+        public Linescore linescore { get; set; }
+        public string venue_w_chan_loc { get; set; }
+        public string first_pitch_et { get; set; }
+        public string away_team_name { get; set; }
+        public Home_Runs home_runs { get; set; }
+        public string time_date_hm_lg { get; set; }
+        public string id { get; set; }
+        public string home_name_abbrev { get; set; }
+        public string tiebreaker_sw { get; set; }
+        public string ampm { get; set; }
+        public string home_division { get; set; }
+        public string home_time_zone { get; set; }
+        public string away_time_zone { get; set; }
+        public string hm_lg_ampm { get; set; }
+        public string home_sport_code { get; set; }
+        public string time_date { get; set; }
+        public Links links { get; set; }
+        public string home_ampm { get; set; }
+        public string game_pk { get; set; }
+        public string venue { get; set; }
+        public string home_league_id { get; set; }
+        public string video_thumbnail { get; set; }
+        public string away_loss { get; set; }
+        public string resume_date { get; set; }
+        public string away_file_code { get; set; }
+        public Losing_Pitcher losing_pitcher { get; set; }
+        public string aw_lg_ampm { get; set; }
+        public Video_Thumbnails video_thumbnails { get; set; }
+        public string time_zone_hm_lg { get; set; }
+        public string away_ampm { get; set; }
+        public string gameday { get; set; }
+        public string away_division { get; set; }
+    }
+
+    public class Broadcast
+    {
+        public Away away { get; set; }
+        public Home home { get; set; }
+    }
+
+    public class Away
+    {
+        [JsonProperty("tv")]
+        private object _tv;
+
+        [JsonIgnore]
+        public string tv
+        {
+            get { return _tv as string; }
+            set { _tv = value; }
+        }
+        [JsonProperty("radio")]
+        private object _radio;
+
+        [JsonIgnore]
+        public string radio
+        {
+            get { return _radio as string; }
+            set { _radio = value; }
+        }
+    }
+
+    public class Home
+    {
+        [JsonProperty("tv")]
+        private object _tv;
+
+        [JsonIgnore]
+        public string tv
+        {
+            get { return _tv as string; }
+            set { _tv = value; }
+        }
+        [JsonProperty("radio")]
+        private object _radio;
+
+        [JsonIgnore]
+        public string radio
+        {
+            get { return _radio as string; }
+            set { _radio = value; }
+        }
+    }
+
+    public class Save_Pitcher
+    {
+        public string id { get; set; }
+        public string last { get; set; }
+        public string saves { get; set; }
+        public string losses { get; set; }
+        public string era { get; set; }
+        public string name_display_roster { get; set; }
+        public string number { get; set; }
+        public string svo { get; set; }
+        public string first { get; set; }
+        public string wins { get; set; }
+    }
+
+    public class Status
+    {
+        public string is_no_hitter { get; set; }
+        public string top_inning { get; set; }
+        public string s { get; set; }
+        public string b { get; set; }
+        public string reason { get; set; }
+        public string ind { get; set; }
+        public string status { get; set; }
+        public string is_perfect_game { get; set; }
+        public string o { get; set; }
+        public string inning { get; set; }
+        public string inning_state { get; set; }
+        public string note { get; set; }
+    }
+
+    public class Winning_Pitcher
+    {
+        public string id { get; set; }
+        public string last { get; set; }
+        public string losses { get; set; }
+        public string era { get; set; }
+        public string number { get; set; }
+        public string name_display_roster { get; set; }
+        public string first { get; set; }
+        public string wins { get; set; }
+    }
+
+    public class Game_Media
+    {
+        [JsonConverter(typeof(SingleValueArrayConverter<Medium>))]
+        public List<Medium> media { get; set; }
+    }
+
+    public class Medium
+    {
+        public string free { get; set; }
+        public string title { get; set; }
+        public string thumbnail { get; set; }
+        public string media_state { get; set; }
+        public DateTime start { get; set; }
+        public string has_mlbtv { get; set; }
+        public string calendar_event_id { get; set; }
+        public string enhanced { get; set; }
+        public string type { get; set; }
+        public string headline { get; set; }
+        public string content_id { get; set; }
+        public string topic_id { get; set; }
+    }
+
+    public class Linescore
+    {
+        public Hr hr { get; set; }
+        public E e { get; set; }
+        public So so { get; set; }
+        public R r { get; set; }
+        public Sb sb { get; set; }
+        [JsonConverter(typeof(SingleValueArrayConverter<Inning>))]
+        public List<Inning> inning { get; set; }
+        public H h { get; set; }
+    }
+
+    public class Hr
+    {
+        public string home { get; set; }
+        public string away { get; set; }
+    }
+
+    public class E
+    {
+        public string home { get; set; }
+        public string away { get; set; }
+    }
+
+    public class So
+    {
+        public string home { get; set; }
+        public string away { get; set; }
+    }
+
+    public class R
+    {
+        public string home { get; set; }
+        public string away { get; set; }
+        public string diff { get; set; }
+    }
+
+    public class Sb
+    {
+        public string home { get; set; }
+        public string away { get; set; }
+    }
+
+    public class H
+    {
+        public string home { get; set; }
+        public string away { get; set; }
+    }
+
+    public class Inning
+    {
+        public string home { get; set; }
+        public string away { get; set; }
+    }
+
+    public class Home_Runs
+    {
+        [JsonConverter(typeof(SingleValueArrayConverter<Player>))]
+        public List<Player> player { get; set; }
+    }
+
+    public class Player
+    {
+        public string std_hr { get; set; }
+        public string hr { get; set; }
+        public string id { get; set; }
+        public string last { get; set; }
+        public string team_code { get; set; }
+        public string inning { get; set; }
+        public string runners { get; set; }
+        public string number { get; set; }
+        public string name_display_roster { get; set; }
+        public string first { get; set; }
+    }
+
+    public class Links
+    {
+        public string away_audio { get; set; }
+        public string wrapup { get; set; }
+        public string preview { get; set; }
+        public string home_preview { get; set; }
+        public string away_preview { get; set; }
+        public string tv_station { get; set; }
+        public string home_audio { get; set; }
+        public string mlbtv { get; set; }
+    }
+
+    public class Losing_Pitcher
+    {
+        public string id { get; set; }
+        public string last { get; set; }
+        public string losses { get; set; }
+        public string era { get; set; }
+        public string number { get; set; }
+        public string name_display_roster { get; set; }
+        public string first { get; set; }
+        public string wins { get; set; }
+    }
+
+    public class Video_Thumbnails
+    {
+        [JsonConverter(typeof(SingleValueArrayConverter<Thumbnail>))]
+        public List<Thumbnail> thumbnail { get; set; }
+    }
+
+    public class Thumbnail
+    {
+        public string content { get; set; }
+        public string height { get; set; }
+        public string scenario { get; set; }
+        public string width { get; set; }
+    }
+    
+}
